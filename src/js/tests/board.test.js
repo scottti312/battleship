4820ac074e66a77cf284462ba4e830086c3991d2
Scott <scott.ti312@gmail.com>
2022-09-19 15:28:37 -0700

    Add ship placement tests

--- src/js/tests/board.test.js
+++ src/js/tests/board.test.js
@@ -1,7 +1,9 @@
-import { initializeBoard } from '../game/board';
+import { Board } from '../game/board';
+import { Coords, Ship } from '../game/ship';
 
 test('Empty 10x10 board created', () => {
-  expect(initializeBoard()).toStrictEqual([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  let testBoard = new Board();
+  expect(testBoard.board).toStrictEqual([[0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -11,4 +13,41 @@ test('Empty 10x10 board created', () => {
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
   [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+});
+
+test('Place horizontal ship', () => {
+  let testBoard = new Board();
+  let testShip = new Ship(4, Coords(6, 6), 'horizontal');
+  testBoard.placeShip(testShip);
+  expect(testBoard.board).toStrictEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+
+});
+
+test('Place vertical ship', () => {
+  let testBoard = new Board();
+  let testShip = new Ship(3, Coords(3, 5), 'vertical');
+  testBoard.placeShip(testShip);
+  console.log(testBoard.board);
+  expect(testBoard.board).toStrictEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+
 });
