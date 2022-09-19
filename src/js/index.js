133b13930128cf8e92d457453afee36da86eb296
Scott <scott.ti312@gmail.com>
2022-09-18 19:02:35 -0700

    Add jest example test

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 console.log('test');
+
+function increment(n) {
+  return ++n;
+}
+
+
+export default increment;
---
+++ src/js/index.test.js
@@ -0,0 +1,9 @@
+import increment from './index.js';
+
+test('Whatever', () => {
+  expect(increment(5)).toBe(6);
+});
+
+test('Whatevers', () => {
+  expect(increment(4)).toBe(5);
+});
