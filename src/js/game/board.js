72ea5847c22c742aae0d11651416165e177c1e5d
Scott <scott.ti312@gmail.com>
2022-09-19 15:28:45 -0700

    Add ship placement

--- src/js/game/board.js
+++ src/js/game/board.js
@@ -1,9 +1,32 @@
-function initializeBoard() {
-  const arr = new Array(10);
-  for (let i = 0; i < 10; i++) {
-    arr[i] = new Array(10).fill(0);
+import { Ship, Coords } from './ship';
+
+class Board {
+  constructor() {
+    const arr = new Array(10);
+    for (let i = 0; i < 10; i++) {
+      arr[i] = new Array(10).fill(0);
+    }
+    this.board = arr;
   }
-  return arr;
+
+  placeShip(ship) {
+    this.placeShipBoardChange(ship);
+  }
+
+  placeShipBoardChange(ship) {
+    let row = ship.headCoords.row;
+    let column = ship.headCoords.column;
+    if (ship.orientation === 'horizontal') {
+      for (let i = column - ship.length + 1; i < column + 1; i++) {
+        this.board[row][i] = 1;
+      }
+    }
+    if (ship.orientation === 'vertical') {
+      for (let i = row; i < row + ship.length; i++) {
+        this.board[i][column] = 1;
+      }
+    }
+  }
 }
 
-export { initializeBoard };
+export { Board };
