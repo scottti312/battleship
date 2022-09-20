93a10062715a061294dbbeb6033af71438df34db
Scott <scott.ti312@gmail.com>
2022-09-20 16:38:16 -0700

    Add test for receiving attack on board

--- src/js/tests/board.test.js
+++ src/js/tests/board.test.js
@@ -30,14 +30,12 @@ test('Place horizontal ship', () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
-
 });
 
 test('Place vertical ship', () => {
   let testBoard = new Board();
   let testShip = new Ship(3, Coords(3, 5), 'vertical');
   testBoard.placeShip(testShip);
-  console.log(testBoard.board);
   expect(testBoard.board).toStrictEqual([
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -51,3 +49,23 @@ test('Place vertical ship', () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
 
 });
+
+test('Hit horizontal ship', () => {
+  let testBoard = new Board();
+  let testShip = new Ship(5, Coords(7, 8), 'horizontal');
+  testBoard.placeShip(testShip);
+  testBoard.receiveAttack(Coords(7, 5));
+  expect(testBoard.board).toStrictEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 1, 2, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+  expect(testShip.hits).toStrictEqual([0, 1, 0, 0, 0]);
+});
+
