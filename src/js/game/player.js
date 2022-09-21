b413758305ddbcd9456ddb2ea17c9489e1e46d1c
Scott <scott.ti312@gmail.com>
2022-09-21 16:55:05 -0700

    Add computer random ship placement

--- src/js/game/player.js
+++ src/js/game/player.js
@@ -1,10 +1,33 @@
 import { Board } from './board.js';
+import { Ship } from './ship.js';
 import Coords from './coords.js';
 
 class Player {
   constructor() {
-    this.whatever = 0;
     this.board = new Board();
+  }
+
+  computerPlaceShip(length) {
+    let positionArr = this.generateRandomCoord();
+    let position = Coords(positionArr[0], positionArr[1]);
+    let orientation = this.randomOrientation();
+
+    while ((orientation == 'vertical' && position[0] > (9 - length)) ||
+      (orientation == 'horizontal' && position[1] < length) ||
+      (this.board.board[position.row][position.column] > 1)) {
+      positionArr = this.generateRandomCoord();
+      position = Coords(positionArr[0], positionArr[1]);
+    }
+
+    let ship = new Ship(length, position, orientation);
+    this.board.placeShip(ship);
+  }
+
+  computerPlaceShips() {
+    for (let i = 2; i < 6; i++) {
+      this.computerPlaceShip(i);
+    }
+    return true;
   }
 
   computerAttack() {
@@ -21,6 +44,14 @@ class Player {
     let randomX = Math.floor(Math.random() * 10);
     let randomY = Math.floor(Math.random() * 10);
     return [randomX, randomY];
+  }
+
+  randomOrientation() {
+    let orientation = Math.floor(Math.random());
+    if (orientation == 1) {
+      return 'horizontal';
+    }
+    return 'vertical';
   }
 }
 
