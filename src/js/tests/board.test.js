1d50de3cff6fc41efbe8892533ef04d9b52a2b00
Scott <scott.ti312@gmail.com>
2022-09-20 17:03:08 -0700

    Add game over test conditions

--- src/js/tests/board.test.js
+++ src/js/tests/board.test.js
@@ -69,7 +69,6 @@ test('Hit horizontal ship', () => {
   expect(testShip.hits).toStrictEqual([0, 1, 0, 0, 0]);
 });
 
-
 test('Miss horizontal ship', () => {
   let testBoard = new Board();
   let testShip = new Ship(5, Coords(7, 8), 'horizontal');
@@ -88,4 +87,66 @@ test('Miss horizontal ship', () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
   expect(testShip.hits).toStrictEqual([0, 1, 0, 0, 0]);
+});
+
+test('All ships sunk. Game over', () => {
+  let testBoard = new Board();
+
+  let testShip = new Ship(5, Coords(7, 8), 'horizontal');
+  testBoard.placeShip(testShip);
+  let testShip1 = new Ship(4, Coords(3, 1), 'vertical');
+  testBoard.placeShip(testShip1);
+  let testShip2 = new Ship(3, Coords(2, 5), 'vertical');
+  testBoard.placeShip(testShip2);
+
+  for (let i = 4; i <= 8; i++)
+    testBoard.receiveAttack(Coords(7, i));
+  for (let i = 3; i <= 6; i++)
+    testBoard.receiveAttack(Coords(i, 1));
+  for (let i = 2; i <= 4; i++)
+    testBoard.receiveAttack(Coords(i, 5));
+
+  expect(testBoard.board).toStrictEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+  expect(testBoard.allShipsSunk()).toBe(true);
+});
+
+test('Not all ships sunk. Not game over', () => {
+  let testBoard = new Board();
+
+  let testShip = new Ship(5, Coords(7, 8), 'horizontal');
+  testBoard.placeShip(testShip);
+  let testShip1 = new Ship(4, Coords(3, 1), 'vertical');
+  testBoard.placeShip(testShip1);
+  let testShip2 = new Ship(3, Coords(2, 5), 'vertical');
+  testBoard.placeShip(testShip2);
+
+  for (let i = 4; i <= 8; i++)
+    testBoard.receiveAttack(Coords(7, i));
+  for (let i = 3; i <= 6; i++)
+    testBoard.receiveAttack(Coords(i, 1));
+  for (let i = 2; i <= 3; i++)
+    testBoard.receiveAttack(Coords(i, 5));
+
+  expect(testBoard.board).toStrictEqual([
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 2, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 1, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 2, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 2, 2, 2, 2, 2, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
+  expect(testBoard.allShipsSunk()).toBe(false);
 });
