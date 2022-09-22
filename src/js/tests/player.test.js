3f9f635861457a5bc1ca3d9ffbde84807c8fa477
Scott <scott.ti312@gmail.com>
2022-09-22 00:07:41 -0700

    Added tests for computer attacks all positions and random ship placements

--- src/js/tests/player.test.js
+++ src/js/tests/player.test.js
@@ -19,20 +19,44 @@ test('Place ship', () => {
     [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]);
 });
 
-test('Random attack one place left', () => {
+// test('Random attack one place left', () => {
+//   let testPlayer = new Player();
+//   let board = [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//   [3, 3, 3, 3, 3, 1, 3, 3, 3, 3],  // there is a ship at [6, 6] here
+//   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//   [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]];
+//   testPlayer.board.board = board;
+//   testPlayer.computerAttack();
+//   expect(testPlayer.board.board).toStrictEqual(
+//     [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+//     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]);
+// });
+
+test('Computer place random ships', () => {
   let testPlayer = new Player();
-  let board = [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 3, 3, 3, 3, 1, 3, 3, 3, 3],  // there is a ship at [6, 6] here
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
-  [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]];
-  testPlayer.board.board = board;
-  testPlayer.computerAttack();
+  expect(testPlayer.computerPlaceShips()).toBe(true);
+  console.log(testPlayer.board.ships);
+  console.log(testPlayer.board.board);
+});
+
+test('Computer attacks all coords', () => {
+  let testPlayer = new Player();
+  for (let i = 0; i < 100; i++)
+    testPlayer.computerAttack();
   expect(testPlayer.board.board).toStrictEqual(
     [[3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
@@ -44,9 +68,4 @@ test('Random attack one place left', () => {
     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
     [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]]);
-});
-
-test('Computer place random ships', () => {
-  let testPlayer = new Player();
-  expect(testPlayer.computerPlaceShips()).toBe(true);
 });
